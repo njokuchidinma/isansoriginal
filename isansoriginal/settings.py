cce8ac90a9aaff73b1c170e4168b1dce5a882671
Zenith-Spark <zenithsparkstation@gmail.com>
2024-11-30 10:21:03 +0100

    changed to dbsqlite

--- isansoriginal/settings.py
+++ isansoriginal/settings.py
@@ -83,16 +83,24 @@ WSGI_APPLICATION = 'isansoriginal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'isansdb',
+#         'USER': 'isans',
+#         'PASSWORD': 'isanspassword',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+#isansadmin123
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'isansdb',
-        'USER': 'isans',
-        'PASSWORD': 'isanspassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
